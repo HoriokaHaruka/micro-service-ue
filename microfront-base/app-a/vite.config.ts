ad49118ed8a583f5eb00a22b40360dbee1768eee
HARUKA <horioka.h@gmail.com>
2025-04-03 14:27:31 +0900

    Initial commit

---
+++ microfront-base/app-a/vite.config.ts
@@ -0,0 +1,18 @@
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import vitePluginSingleSpa from "vite-plugin-single-spa";
+
+// https://vite.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    vitePluginSingleSpa({
+      type: "mife",
+      serverPort: 4101,
+      spaEntryPoints: ["./src/spa.tsx"],
+    }),
+  ],
+  server: {
+    hmr: false,
+  },
+});
